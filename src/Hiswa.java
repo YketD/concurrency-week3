9299548539b75aaba31f4e07eca932c070a69da3
Yke ten Donkelaar <yketd@hotmail.com>
2016-10-09 21:43:10 +0200

    finished

--- src/Hiswa.java
+++ src/Hiswa.java
@@ -9,11 +9,13 @@ public class Hiswa
 {
     private static final int MAX_VISITORS = 10;
     private static final int MAX_BUYERS = 4;
+    private int BUYERS_ENTERED = 0;
 
     private int nrOfVisitors = 0;
     private int nrOfBuyers = 0;
 
     private Condition allowMoreVisitors;
+    private Condition hiswaIsEmpty;
 
     Lock lock;
 
@@ -21,6 +23,7 @@ public class Hiswa
     {
         lock = new ReentrantLock();
         allowMoreVisitors = lock.newCondition();
+        hiswaIsEmpty = lock.newCondition();
     }
 
 
@@ -31,7 +34,7 @@ public class Hiswa
         try {
             System.out.println("[HISWA] A buyer entered, waiting for everyone to leave");
             nrOfBuyers++;
-
+            hiswaIsEmpty.await();
         } finally {
             lock.unlock();
         }
@@ -48,6 +51,7 @@ public class Hiswa
             nrOfVisitors++;
             System.out.println("[HISWA] A visitor entered, total visitors: " + nrOfVisitors);
 
+
         } finally {
             lock.unlock();
         }
@@ -58,13 +62,26 @@ public class Hiswa
         lock.lock();
 
         try {
-            if (Thread.currentThread() instanceof Kijker)
+            if (Thread.currentThread() instanceof Kijker){
                 nrOfVisitors--;
-            else if (Thread.currentThread() instanceof Koper)
+                if (nrOfVisitors == 0){hiswaIsEmpty.signal();}
+            }
+            else if (Thread.currentThread() instanceof Koper) {
                 nrOfBuyers--;
+                BUYERS_ENTERED++;
+                if (BUYERS_ENTERED == MAX_BUYERS) {
+                    allowMoreVisitors.signalAll();
+                    BUYERS_ENTERED = 0;
+                }   else if(nrOfBuyers > 1){
+                    hiswaIsEmpty.signal();
+                }
+            }
+
 
             if (!isBuyerWaiting())
                 allowMoreVisitors.signalAll();
+
+
 
             System.out.println("[HISWA] " + Thread.currentThread().getName() + " left, remaining visitors: " + nrOfVisitors);
 
